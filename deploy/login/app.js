86525dfc24cbceecd2d7275becd964dc01667d99
s1119858711 <1119858711@qq.com>
2021-02-03 19:15:45 +0800

    测试npm项目是否可以发

---
+++ deploy/login/app.js
@@ -0,0 +1,57 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+app.use(async function(ctx){
+	if(ctx.path == '/login'){
+		// console.log(ctx.request.body.account);
+		// console.log(ctx.request.body.pwd);
+		if(ctx.request.body.account == 'new' && ctx.request.body.pwd == '123'){
+			ctx.body = '登陆成功'
+		}else{
+			ctx.body = ctx.request.body.account +'和'+ctx.request.body.pwd
+		}
+	}
+})
+
+
+module.exports = app
